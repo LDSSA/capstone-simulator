c65abf072ee2e1cdbbd27cf8623f6d55e1b976d8
Sam Hopkins <sam@daredata.engineering>
2018-02-18 23:06:22 +0000

    fixing the show bad request

--- show-bad-requests.py
+++ show-bad-requests.py
@@ -1,3 +1,4 @@
+import sys
 from pprint import pprint
 import pandas as pd
 import json
@@ -7,13 +8,27 @@ df = pd.concat([
     pd.read_csv('test_no_give_outcomes.csv')
 ]).set_index('id')
 
-with open('state.json') as fh:
-    state = json.load(fh)
+if sys.argv[1] == 'observation':
+    with open('state.json') as fh:
+        state = json.load(fh)
 
-for app_name, responses in state.items():
-    bads = list(filter(lambda x: x[1] == -1, responses))
-    print('========================================================')
-    print(app_name)
-    for obs_id, _ in bads[0:10]:
-        pprint(df.loc[obs_id].to_dict())
-    print('\n\n\n\n')
+    for app_name, responses in state.items():
+        bads = list(filter(lambda x: x[1] == -1, responses))
+        print('========================================================')
+        print(app_name)
+        for obs_id, _ in bads[0:10]:
+            pprint(df.loc[obs_id].to_dict())
+        print('\n\n\n\n')
+
+
+elif sys.argv[1] == 'true-outcome':
+
+    with open('true_outcome_state.json') as fh:
+        state = json.load(fh)
+
+    for app_name, responses in state.items():
+        bads = list(filter(lambda x: x[1] == -1, responses))
+        if bads:
+            print('========================================================')
+            print(app_name)
+            pprint(bads)
